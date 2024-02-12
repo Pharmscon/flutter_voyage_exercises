e8cbada54c5fcb7d783b5bd8b3b3c1bda04cb646
Pharmscon <successchikale@gmail.com>
2024-02-12 22:12:06 +0100

    first int

--- bin/hello_dart_project.dart
+++ bin/hello_dart_project.dart
@@ -1,5 +1,20 @@
-import 'package:hello_dart_project/hello_dart_project.dart' as hello_dart_project;
+void main() {
+  print("Hello, Voyagers");
+  var name = "Pharmscon";
+  print(name);
+  var x = 10;
+  var y = 50;
+  var value = x / 2;
+  print(value);
+  x = 10;
+  y = 50;
+  if (x <= 5 && y >= 100) ;
+  print(true);
+  print(perimeterofarectangle(6, 5));
+}
 
-void main(List<String> arguments) {
-  print('Hello world: ${hello_dart_project.calculate()}!');
+num perimeterofarectangle(l, w) {
+  num l = 6;
+  num w = 5;
+  return 2 * (l + w);
 }
